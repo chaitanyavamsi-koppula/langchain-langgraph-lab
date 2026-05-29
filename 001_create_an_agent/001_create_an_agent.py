b11b27c6ce3f19fb4bde35c289fc2aec66fae7ff
chaitanyavamsi-koppula <koppula.chaitanyavamsi@gmail.com>
2026-05-30 02:35:59 +0530

    feat: add requirements.txt and create weather agent script

---
+++ 001_create_an_agent/001_create_an_agent.py
@@ -0,0 +1,34 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain.agents import create_agent
+
+
+def get_weather(city: str) -> str:
+    """
+    Get weather for a given city.
+
+    :param city: takes city as input
+    :type city: str
+    :return: returns weather_info in the provided city
+    :rtype: str
+    """
+    return f"It's always sunny in {city}!"
+
+
+agent = create_agent(
+    model="google_genai:gemini-2.5-flash-lite",
+    tools=[get_weather],
+    system_prompt="you are a helpful assistant",
+)
+
+result = agent.invoke(
+    {
+        "messages": [
+            {"role": "user", "content": "What's is the weather like in san fransisco"}
+        ]
+    }
+)
+
+print(result["messages"][-1].content_blocks)
